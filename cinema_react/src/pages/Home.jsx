dd741b5854faa22b757d7729aea43d8ebc1367f8
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2024-05-15 17:41:39 -0300

    UP

--- cinema_react/src/pages/Home.jsx
+++ cinema_react/src/pages/Home.jsx
@@ -5,9 +5,25 @@ const moviesURL = import.meta.env.VITE_API;
 const apikey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
-    console.log(moviesURL, apikey);
+    const [topMovies, setTopMovies] = useState([]);
 
-    return <div>Home</div>;
+    const getTopRatedMovies = async (url) => {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        setTopMovies(data.results);
+    };
+
+    useEffect(() => {
+
+        const topRatedUrl = `${moviesURL}top_rated?${apikey}`;
+        
+        getTopRatedMovies(topRatedUrl);
+    },[])
+
+    return (
+    <div>{topMovies && topMovies.map((movie) => <p>{movie.title}</p>)}</div>    
+    );
 };
 
 export default Home;
