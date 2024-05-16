cc81f1b44027ecbda20cf96826db71e85b3e158b
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2024-05-15 21:12:49 -0300

    UP

--- cinema_react/src/components/MovieCard.jsx
+++ cinema_react/src/components/MovieCard.jsx
@@ -4,11 +4,11 @@ import {FaStar} from 'react-icons/fa'
 
 const imageUrl = import.meta.env.VITE_IMG
 
-const MovieCard = ({movie, showLink = true}) => {
+const MovieCard = ({ movie, showLink = true}) => {
     return (
         // Já vai puxar as informações da API e fazer com que fique correto nome, imagem e descrição
         <div className="movie-card">
-            <img src="{imageUrl + movie.poster_path}" alt={movie.title} />
+            <img src={imageUrl + movie.poster_path} alt={movie.title} />
             <h2>{movie.title}</h2>
             {/* Para fazer com que apareça */}
             <p>
