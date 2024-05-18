48a7072ffd7e9a6626cf5184f8d675eda83e919b
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2024-05-18 16:53:49 -0300

    up

--- cinema_react/src/components/Navbar.jsx
+++ cinema_react/src/components/Navbar.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
-
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
 import "./Navbar.css";
 
 const Navbar = () => {
-    const [search, setSearch] = useState("")
+    const [search, setSearch] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      
+      if(!search) return;
+
+      navigate(`/search?q=${search}`);
+      setSearch("");
+    }
 
     return (
         <nav id="navbar">
@@ -14,7 +23,7 @@ const Navbar = () => {
             {/*Pegar o icone que eu achar mais da preferencia*/}
           <Link to="/"><BiCameraMovie />Cinema do DanDan</Link>
         </h2>
-        <form>
+        <form onSubmit={handleSubmit}>
 
             <input type="text" placeholder="Busque um filme"
              onChange={(e) => setSearch(e.target.value)}
