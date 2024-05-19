745a49728b66e88643bc7c6edac9019a233f4446
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2024-05-19 17:52:57 -0300

    up

--- cinema_react/src/pages/Search.jsx
+++ cinema_react/src/pages/Search.jsx
@@ -2,42 +2,40 @@ import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import MovieCard from "../components/MovieCard";
 
-const searchURL = import.meta.env.VITE_SEARCH
-const apiKey = import.meta.env.VITE_API_KEY
+const searchURL = import.meta.env.VITE_SEARCH;
+const apiKey = import.meta.env.VITE_API_KEY;
 
-import "./MovieGrid.css";
+// Essa importação estava dando error na aplicação, por disso desativa por enquanto
+// import "./MoviesGrid.css";
 
 const Search = () => {
-    const [searchParams] = useSearchParams();       
+  const [searchParams] = useSearchParams();
 
-    const [movies, setMovies] = useState([]);
-    const query = searchParams.get("q");
+  const [movies, setMovies] = useState([]);
+  const query = searchParams.get("q");
 
-    const getSearchedMovies = async (url) => {
-        const res = await fetch(url);
-        const data = await res.json();
+  const getSearchedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setMovies(data.results);
+  };
 
-        setTopMovies(data.results);
-    };
+  useEffect(() => {
+    const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`;
+    getSearchedMovies(searchWithQueryURL);
+  }, [query]);
 
-    useEffect(() => {
-        const searchWithQueryURL = `${searchURL}?${apikey}&query=${query}`;
-         
-        getSearchedMovies(searchWithQueryURL);
-    },[]);
-
-    return (
-        <div className="container">
-        <h2 className="title">
-            Resultado para:<span className="query-text">{query}</span>
-        </h2>
-        <div className="movies-container">
-            {topMovies.length === 0 && <p>Carregando...</p>}
-            {topMovies.length > 0 &&
-                topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
-        </div>
-    </div>     
-    );
+  return (
+    <div className="container">
+      <h2 className="title">
+        Resultados para: <span className="query-text">{query}</span>
+      </h2>
+      <div className="movies-container">
+        {movies.length > 0 &&
+          movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
